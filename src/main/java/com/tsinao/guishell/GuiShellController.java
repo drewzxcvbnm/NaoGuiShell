5d690cf70acd08a31219bfccbeb5e794b467f628
andris.zacs <andris.zacs@accenture.com>
2021-11-15 10:02:24 +0200

    refactor

--- src/main/java/com/tsinao/guishell/GuiShellController.java
+++ src/main/java/com/tsinao/guishell/GuiShellController.java
@@ -1,19 +1,21 @@
 package com.tsinao.guishell;
 
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.Callable;
+import java.util.regex.Pattern;
 
 
 public class GuiShellController {
 
-    public static final String NAOPPTX_EXE = "naopptx\\.exe";
-    public static final String BEHAVIOR_EXE = "behavior\\.exe";
-    public static final String PPTX = ".*\\.pptx";
+    private static final Pattern IP_PATTERN = Pattern.compile("^(\\d|\\d{2,3})\\.(\\d|\\d{2,3})\\.(\\d|\\d{2,3})\\.(\\d|\\d{2,3})$");
+    private static final String NAOPPTX_EXE = "naopptx\\.exe";
+    private static final String BEHAVIOR_EXE = "behavior\\.exe";
+    private static final String PPTX = ".*\\.pptx";
     @FXML
     public Label foundNaopptx;
     @FXML
@@ -24,6 +26,13 @@ public class GuiShellController {
     public TextField naoIp;
     @FXML
     public CheckBox noNet;
+    @FXML
+    public Button runNaopptx;
+
+    @FXML
+    public void initialize() {
+        setRunNaoPptxPredicate();
+    }
 
     @FXML
     public void findNaopptx() {
@@ -44,7 +53,7 @@ public class GuiShellController {
     }
 
     @FXML
-    public void runNaopptx() {
+    public void runNaoPptxCommand() {
         String command = String.format("%s --ip %s --pr %s %s", foundNaopptx.getText(), naoIp.getText(),
                 foundPptx.getText(), noNet.isSelected() ? "--no-inet" : "");
         try {
@@ -57,4 +66,19 @@ public class GuiShellController {
             alert.show();
         }
     }
+
+    private void setRunNaoPptxPredicate() {
+        Callable<Boolean> booleanCallable = () -> shouldDisableRunNaoPptx(foundPptx, foundPptx, naoIp);
+        var booleanBinding = Bindings.createBooleanBinding(booleanCallable, foundPptx.textProperty(),
+                foundPptx.textProperty(), naoIp.textProperty());
+        runNaopptx.disableProperty().bind(booleanBinding);
+    }
+
+    private boolean shouldDisableRunNaoPptx(Label foundNaoPptx, Label foundPptx, TextField naoIP) {
+        if (!IP_PATTERN.matcher(naoIP.getText()).matches()) {
+            return true;
+        }
+        return Arrays.asList(foundNaoPptx.getText(), foundPptx.getText()).contains("");
+    }
+
 }
